688e96db5b3393b050163914c332b766157706b1
chenzf <chenzf@pvc123.com>
2021-01-11 08:42:57 +0800

    fix(master): 增加HttpClient

--- src/Controller.php
+++ src/Controller.php
@@ -13,6 +13,17 @@ abstract class Controller
 {
     protected $middleware = [];
 
+    /**
+     * 注册initialize方法
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        if (method_exists($this, '_initialize')) {
+            $this->_initialize();
+        }
+    }
+
     // 获取中间件
     public function getMiddleware(): array
     {
