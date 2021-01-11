782a7ed7d7939d699d5f945cd77c27fcea3d3d97
chenzf <chenzf@pvc123.com>
2021-01-11 10:27:19 +0800

    feat(master): 重构项目

--- src/util/Loader.php
+++ src/util/Loader.php
@@ -15,13 +15,24 @@ class Loader
 {
     /**
      * @param $serviceClass
+     * @param bool $isSingleton
      * @return mixed
      */
-    public static function service($serviceClass)
+    public static function service($serviceClass, $isSingleton = true)
     {
-        if (! Container::getContainer()->has($serviceClass)) {
-            Container::getContainer()->bind($serviceClass, $serviceClass, true);
+        if (!Container::getContainer()->has($serviceClass)) {
+            Container::getContainer()->bind($serviceClass, $serviceClass, $isSingleton);
         }
         return Container::getContainer()->get($serviceClass);
+    }
+
+
+    /**
+     * @param $serviceClass
+     * @return mixed
+     */
+    public static function singleton($serviceClass)
+    {
+        return static::service($serviceClass, true);
     }
 }
